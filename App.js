91df350cd7e4b9978629f2b9b2d11b4dbdd2af7f
sam <3161728@gmail.com>
2020-12-17 21:11:11 +0900

    Test Layout with Flexbox

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: 'black',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  yellowView:{
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor: 'blue',
   },
 });
